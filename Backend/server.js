23acbd57324441356732f53b7a988ec694bfaae8
sonam kumari <135491610+ITSSONAM-0@users.noreply.github.com>
2025-12-05 19:21:22 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -3,20 +3,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const server_config = require("./config/server.config");
 
+const userRoutes = require("./Routes/user.routes"); // <-- Correct path
+
 const app = express();
 app.use(express.json());
 
-app.use("/uploads", express.static("uploads")); // serve uploaded files (dev only)
+app.use("/uploads", express.static("uploads"));
 
 app.use("/api/users", userRoutes);
-
-
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.log("MongoDB Error:", err));
-
-
 
 app.listen(server_config.PORT, () => {
   console.log("Server running on port", server_config.PORT);
